c2b1d0387263c15347037b0e046b975d5f2dc0ad
Michael Engelbrecht <michaelndani@gmail.com>
2021-12-20 07:12:28 +1100

    Initial template following instructions from Python project packaging tutorial:
    Main tutorial: https://packaging.python.org/en/latest/tutorials/packaging-projects/
    Console script entry point as per: https://setuptools.pypa.io/en/latest/userguide/entry_point.html
    
    BUILD STEPS:
    
    cd "/Users/michael/Documents/SW - Production"
    mkdir disk-growth
    cd disk-growth
    git init
    git commit -m --allow-empty "Initial commit"
    
    mkdir tests
    mkdir src
    touch README.md
    nano LICENSE
        [pasted MIT Licence - from: https://choosealicense.com/licenses/mit/]
    nano pyproject.toml
        [pasted from tutorial]
    nano setup.cfg
        [pasted from tutorial]
    nano setup.py
        [pasted DUMMY from: https://stackoverflow.com/a/60885212]
    nano .gitignore
        *.pyc
        .DS_Store
        .vscode
        __pycache__
        *.egg-info
        vinv/
    
    mkdir src/example_package
    cd src/example_package
    nano __init__.py
        [pasted from tutorial]
    nano __main__.py
        [pasted from tutorial]
    
    git add .
    git commit -m "Initial template following instructions from Python project packaging tutorial: ..."
    
    cd ..    # back to src folder
    python3 -m example_package    # WORKS!

---
+++ setup.py
@@ -0,0 +1,6 @@
+# DUMMY file created for backwards compatibility with some setup commands - see: https://stackoverflow.com/a/60885212
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
